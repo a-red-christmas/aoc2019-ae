ceb384d86b8f16655d4c876e60ae34ee464c5685
Allan Eising <allan@eising.dk>
2019-12-04 10:28:41 +0100

    Completed day 2 part 2

--- 04/02.py
+++ 04/02.py
@@ -2,7 +2,6 @@
 import re
 
 
-
 class PasswordChecker(object):
     """ Object to check passwords conforming to puzzle """
 
@@ -12,30 +11,21 @@ class PasswordChecker(object):
 
     def check_samedigit(self, pw: str) -> bool:
         """ We check for repeating digits """
-        res = re.compile(r'.*(\d)\1.*')
-        res_4 = re.compile(r'.*(\d)\1\1\1.*')
-        res_6 = re.compile(r'.*(\d)\1\1\1\1\1.*')
-        if res.match(pw) is not None:
-            # Check what digit it is:
-            digit = res.match(pw).group(1)
-            if res_4.match(pw) is None:
-                # There isn't four of it, so let's check if there's three
-                if f'{digit * 3}' in pw:
-                    # There is not four, but there is three
-                    return False
-                else:
-                    # There is in fact two
-                    return True
-            else:
-                # There is four, but is there five?
-                if f'{digit * 5}' in pw:
-                    return False
-                else:
-                    return True
+        res = re.compile(r'(\d)\1')
+        groups = res.findall(pw)
 
-            return True
-        else:
-            return False
+        isvalid = False
+
+        if len(groups) > 0:
+            invalids = []
+            for digit in groups:
+                if f'{digit * 3}' in pw:
+                    invalids.append(digit)
+
+            if len(invalids) < len(groups):
+                isvalid = True
+
+        return isvalid
 
     def check_increase(self, pw: str) -> bool:
         """ We check that digits do not decrease """
@@ -50,12 +40,11 @@ class PasswordChecker(object):
                     decrease = True
                     break
 
-        if decrease == True:
+        if decrease is True:
             return False
 
         else:
             return True
-
 
     def process(self):
         """ Process the range of passwords """
@@ -69,8 +58,6 @@ class PasswordChecker(object):
                 matching.append(pw)
 
         return len(matching)
-
-
 
 
 if __name__ == '__main__':
